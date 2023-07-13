23c200e313d19b6d6e4c9f2e4713c01faeaed236
Erjoh <erjik1902@gmail.com>
2023-07-13 13:24:38 +0600

    js3 homework

---
+++ script.js
@@ -0,0 +1,127 @@
+// ====================1====================
+
+// function checkUser(user){
+//     if (user === 'admin'){
+//         return 'welcome, admin'
+//     } else {
+//         return 'not correct user'
+//     }
+// }
+//
+// console.log(checkUser('admiasas'))
+
+// ====================1====================
+
+// ====================2====================
+
+// function checkMonth(number){
+//     if (number >=1 && number <=28 ){
+//         return `today is ${number} of february`
+//     } else {
+//         return `on february only 28 day, your day is ${number} of another month`
+//     }
+// }
+//
+// console.log(checkMonth(30))
+
+// ====================2====================
+
+// ====================3====================
+
+// function checkEquals(number){
+//     if (number  === 50){
+//         return 'number is equals of 50'
+//     } else {
+//         return 'number is not correct'
+//     }
+// }
+//
+// console.log(checkEquals(50))
+
+// ====================3====================
+
+// ====================4====================
+
+// function checkAge(user, age ){
+//     if ( age >= 30 && age <= 60){
+//         return `${user} ${age} years old, welcome`
+//     } else {
+//         return `${user} ${age} years old, rejected`
+//     }
+// }
+//
+// console.log(checkAge( 'Erjoh', 30))
+
+// ====================4====================
+
+// ====================5====================
+
+// function checkMinmax(user){
+//     if (user.length <=3 && user.length >=5 ){
+//         return `${user} name is accepted`
+//     } else {
+//         return `${user} name is too long`
+//     }
+// }
+//
+// console.log(checkMinmax('sdaffsdf'))
+
+// ====================5====================
+
+// ====================6====================
+
+// function checkType(number) {
+//     if (typeof number === 'number') {
+//         return 'number'
+//     } else{
+//         return 'string'
+//     }
+// }
+//
+// console.log(checkType('20'))
+
+// ====================6====================
+
+// ====================7====================
+
+// function checkAge(age){
+//     if (typeof (age) === 'number'){
+//         if (age >= 23){
+//             return 'welcome'
+//         } else {
+//             return 'must be older then 23'
+//         }
+//     } else 'need a digit'
+// }
+//
+// console.log(checkAge(23))
+
+// ====================7====================
+
+// ====================8====================
+
+// function checkWeek (day) {
+//     if (typeof (day) === 'number'){
+//         if (day === 1) {
+//             return 'today is monday'
+//         } else if (day === 2) {
+//             return 'today is tuesday'
+//         } else if (day === 3) {
+//             return 'today is wednesday'
+//         } else if (day === 4) {
+//             return 'today is thursday'
+//         } else if (day === 5) {
+//             return 'today is friday'
+//         } else if (day === 6 || day === 7) {
+//             return 'weekend'
+//         } else {
+//             return 'max days of weeks 7'
+//         }
+//     } else {
+//         return 'pick a number'
+//     }
+// }
+//
+// console.log(checkWeek(7))
+
+// ====================8====================
